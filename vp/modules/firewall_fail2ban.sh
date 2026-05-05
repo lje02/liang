bf8089b6b3ebce6cd2ae7ca035e7f4d32ee0b48d
lje02 <154250786+lje02@users.noreply.github.com>
2026-05-05 17:14:34 -0400

    更新 firewall_fail2ban.sh

--- vp/modules/firewall_fail2ban.sh
+++ vp/modules/firewall_fail2ban.sh
@@ -9,6 +9,7 @@ if [ -z "$VPS_COMMON_LOADED" ]; then
 fi
 detect_os
 check_dependencies
+
 # -------- 防火墙 --------
 detect_firewall() {
     if command -v ufw &>/dev/null && ufw status | grep -q "Status: active"; then
