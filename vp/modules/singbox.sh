f38cd539c44104acb550c32cfb379ed25d3deee8
lje02 <154250786+lje02@users.noreply.github.com>
2026-05-11 13:01:06 -0400

    更新 singbox.sh

--- vp/modules/singbox.sh
+++ vp/modules/singbox.sh
@@ -5,11 +5,11 @@ if [ -z "$VPS_COMMON_LOADED" ]; then
 fi
 
 if command -v ssb &>/dev/null; then
-    printf "${GREEN}正在进入 sing-box 管理界面...${NC}\n"
+    printf "${GREEN}正在进入 sing-box 管理界面......${NC}\n"
     sleep 1
     ssb
     echo ""
-    #printf "${YELLOW}ssb 已退出。${NC}\n"
+    printf "${YELLOW}正在退出......${NC}\n"
     sleep 1
 else
     printf "${RED}未安装 sing-box脚本。请先使用菜单中的“安装/重装”功能。${NC}\n"
