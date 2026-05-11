e3193898e36bbd2eb9d839aa6a945254c69ae999
lje02 <154250786+lje02@users.noreply.github.com>
2026-05-11 12:59:36 -0400

    更新 singbox.sh

--- vp/modules/singbox.sh
+++ vp/modules/singbox.sh
@@ -10,6 +10,7 @@ if command -v ssb &>/dev/null; then
     ssb
     echo ""
     #printf "${YELLOW}ssb 已退出。${NC}\n"
+    sleep 1
 else
     printf "${RED}未安装 sing-box脚本。请先使用菜单中的“安装/重装”功能。${NC}\n"
 fi
